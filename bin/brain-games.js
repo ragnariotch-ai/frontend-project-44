01344b58363dade339797e928f2d36c0ce0531d1
Sarsela <an.kurnosova@gmail.com>
2026-03-30 13:45:50 +0700

    qwe

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 // sudo ln -s $(which npm) /usr/bin/npm
 
-console.log('Welcome to the Brain Games!');
+//console.log('Welcome to the Brain Games!');
+import {greetUser} from '../src/cli.js';
+greetUser();
